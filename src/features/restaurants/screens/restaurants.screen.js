cfd323686eea6b0790cef4b0dfb90f3c8c49786c
your name <your e-mail>
2021-04-15 01:15:45 +0300

    first part

---
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components/native";
+import { FlatList } from "react-native";
+import { Searchbar } from "react-native-paper";
+import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+import { Spacer } from "../../../components/spacer/spacer.component";
+import { SafeArea } from "../../../components/utility/safe-area.componenet";
+const SearchContainer = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+const RestaurantList = styled(FlatList).attrs({
+  contentContainerStyle: {
+    padding: 16,
+  },
+})``;
+
+export const RestaurantsScreen = () => {
+  return (
+    <SafeArea>
+      <SearchContainer>
+        <Searchbar />
+      </SearchContainer>
+      <RestaurantList
+        data={[{ name: 1 }, { name: 2 }, { name: 3 }, { name: 4 }]}
+        renderItem={() => (
+          <Spacer position="bottom" size="large">
+            <RestaurantInfoCard />
+          </Spacer>
+        )}
+        keyExtractor={(item) => item.name}
+      />
+    </SafeArea>
+  );
+};
